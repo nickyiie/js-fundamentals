2c4500bf2498db71c5b9a4cd47f1768bb94e0368
Nicteha <nictehapinkus@gmail.com>
2020-05-12 00:30:52 +0000

    was messing around but back to original

--- chorus-while.js
+++ chorus-while.js
@@ -1,10 +1,10 @@
 const chorus = "Let's Dance";
 let repeat = 0;
 while (repeat <10) {
-  if (repeat === 5) {
+  if (repeat === 4) {
     console.log("*change key*");
-  } 
-  console.log(chorus);
+  } else { console.log(chorus);
+  }
   repeat++;
 }
 console.log("Until the sun comes up!")
