475ff71d7cef1bfaf7e8d94c5354f250c02f4f6f
Nicteha <nictehapinkus@gmail.com>
2020-05-13 04:19:08 +0000

    tinkering I suppose

--- hello.js
+++ hello.js
@@ -6,3 +6,16 @@ sayHello("Nicky");
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHelloToConsole('John');
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello ('John');
+
+console.log(greeting);
